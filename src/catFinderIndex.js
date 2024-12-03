b2e97046f39644639fedaa5be5f019f0590a846c
GratielaC <gratiela.chirtop@yahoo.com>
2024-12-03 16:53:57 +0200

    commit4

---
+++ src/catFinderIndex.js
@@ -0,0 +1,23 @@
+// imports
+import { getCatsBreeds, fetchCatByBreed } from './jsFiles/catFinder/cat-api.js';
+import { getElement } from './jsFiles/getElement.js';
+
+// variables
+export const breedSelect = getElement('.breed-select');
+export const loader = getElement('.loader-container');
+export const catInfo = getElement('.cat-info');
+export const breedSelectContainer = getElement('.breed-select-container');
+
+const showBreedInfo = e => {
+  const catId = e.target.value;
+  const url = `https://api.thecatapi.com/v1/images/search?breed_ids=${catId}`;
+
+  breedSelectContainer.classList.add('hide');
+  loader.classList.remove('hide');
+  catInfo.innerHTML = '';
+  
+  fetchCatByBreed(url);
+};
+
+window.addEventListener('DOMContentLoaded', getCatsBreeds);
+breedSelect.addEventListener('change', showBreedInfo);
